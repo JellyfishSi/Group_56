f51d8e79b3ff5b48ce269226ef65ccae362b0504
JellyfishSi <2960946S@student.gla.ac.uk>
2025-03-19 00:59:59 +0000

    Fixing errors in build.sh

--- build.sh
+++ build.sh
@@ -12,16 +12,9 @@ pip install --upgrade pip
 # 安装依赖
 pip install -r requirements.txt
 
-# 确保 gunicorn 安装在系统路径
-pip install gunicorn --system
-export PATH=$PATH:/opt/render/project/src/venv/bin
-
 # 数据库迁移
 python manage.py makemigrations
 python manage.py migrate
 
 # 收集静态文件
 python manage.py collectstatic --noinput
-
-# 打印 gunicorn 位置（调试用）
-which gunicorn
